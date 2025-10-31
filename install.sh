4dfc513b7bf49af4aaf86f9c6929c21566b26d59
김예환 Ye-Hwan Kim (Sam) <yehwankim23@gmail.com>
2025-11-01 00:25:02 +0900

    Add files

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+brew install \
+1password \
+aldente \
+alt-tab \
+bartender \
+bat \
+chrome-remote-desktop-host \
+cloudflare-warp \
+discord \
+fastfetch \
+fzf \
+ghostty \
+gimp \
+git \
+git-credential-manager \
+go \
+google-chrome \
+google-drive \
+jetbrains-toolbox \
+linearmouse \
+logi-options+ \
+musescore \
+node@24 \
+notion \
+obs \
+onlyoffice \
+python@3.13 \
+rectangle \
+sublime-text \
+temurin@21 \
+visual-studio-code \
+vlc \
+vmware-fusion \
+zoom \
+zoxide
